c9a3de4b28f8575317f4b290172b8383d2c4d509
Nobody150 <philps1997@gmail.com>
2017-12-18 16:39:34 +0100

    Klassen erstellt

---
+++ src/lagerverwaltung/Bestellbestaetigung.java
@@ -0,0 +1,14 @@
+package lagerverwaltung;
+
+public class Bestellbestaetigung {
+	
+	private boolean ausgefuehrt;
+	private double gesamtpreis;
+	
+	public void Bestellbestaetigung (boolean ausgefuehrt, double gesamtpreis) {
+		this.ausgefuehrt=ausgefuehrt;
+		this.gesamtpreis=gesamtpreis;
+	}
+	
+
+}
