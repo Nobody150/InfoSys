877140eb333b1af54f9193366ca4755851fda123
SkyJumperNr1 <34654529+SkyJumperNr1@users.noreply.github.com>
2018-01-15 14:37:57 +0100

    Jakobs Kommentare hoffentlich

--- src/lagerverwaltung/Lagerverwaltung.java
+++ src/lagerverwaltung/Lagerverwaltung.java
@@ -26,105 +26,162 @@ public class Lagerverwaltung {
 		}
 	}
 
+	/**
+	 * Erteilt einem Mitarbeiter die Berechtigung Wareneingänge zu buchen und
+	 * Bestellungen auszuführen.
+	 * 
+	 * @param mitarbeiter
+	 *            Der Mitarbeiter dem die Berechtigung erteilt werden soll.
+	 */
 	public void berechtigungErteilen(Mitarbeiter mitarbeiter) {
-		berechtigteMitarbeiter.add(mitarbeiter.getID());
-		writer.println(LocalDate.now() + " " + " Berechtigung erteilt an: " + mitarbeiter.getID() + " ("
-				+ mitarbeiter.getName() + ")");
-
-	}
-
-	public void berechtigungZurueckziehen(Mitarbeiter mitarbeiter) {
-		berechtigteMitarbeiter.remove(mitarbeiter.getID());
-		writer.println(LocalDate.now() + " " + " Berechtigung entzogen von: " + mitarbeiter.getID() + " ("
-				+ mitarbeiter.getName() + ")");
-
+		if (mitarbeiter != null) {
+			if (!berechtigteMitarbeiter.contains(mitarbeiter.getID())) {
+				berechtigteMitarbeiter.add(mitarbeiter.getID());
+				writer.println(LocalDate.now() + " Berechtigung erteilt an: " + mitarbeiter.getID() + " ("
+						+ mitarbeiter.getName() + ")");
+			} else {
+				System.out.println("Dieser Mitarbieter hat bereits eine Berechtigung");
+			}
+		} else {
+			System.out.println("Mitarbieter existiert nicht");
+		}
 	}
 
 	/**
-	 * Gibt den Aktuellen Lagerbestand mit den Namen des Artikels.
+	 * Entzieht einem Mitarbeiter die Berechtigung Wareneingänge zu buchen und
+	 * Bestellungen auszuführen.
+	 * 
+	 * @param mitarbeiter
+	 *            Der Mitarbeiter dem die Berechtigung entzogen werden soll.
 	 */
-	public void lagerbestandAusgeben() {
-		Artikel artikel;
-		for (Lagerposten lposten : lagerposten) {
-			artikel = lposten.getArtikel();
-			System.out.println(LocalDate.now() + " " + "Der Bestand für den Artikel: " + artikel.getID() + " ("
-					+ artikel.getName() + ")" + " ist: " + lposten.getLagerbestand());
+	public void berechtigungZurueckziehen(Mitarbeiter mitarbeiter) {
+		if (mitarbeiter != null) {
+			if (berechtigteMitarbeiter.contains(mitarbeiter.getID())) {
+				berechtigteMitarbeiter.remove(mitarbeiter.getID());
+				writer.println(LocalDate.now() + " Berechtigung entzogen von: " + mitarbeiter.getID() + " ("
+						+ mitarbeiter.getName() + ")");
+			} else {
+				System.out.println("Dieser Mitarbeiter hatte gar keine Berechtigung");
+			}
+		} else {
+			System.out.println("Mitarbieter existiert nicht");
 		}
 	}
 
-	public void wareneingangBuchen(Mitarbeiter mitarbeiter, Artikel artikel, int bestand, double preis) {
-		if (berechtigteMitarbeiter.contains(mitarbeiter.getID())) {
-			boolean vorhanden = false;
+	/**
+	 * Gibt den aktuellen Lagerbestand mit den Namen des Artikel aus.
+	 */
+	public void lagerbestandAusgeben() {
+		if (lagerposten != null) {
+			Artikel artikel;
 			for (Lagerposten lposten : lagerposten) {
-				if (lposten.getArtikel().equals(artikel)) {
-					vorhanden = true;
-					lposten.setLagerbestand(lposten.getLagerbestand() + bestand);
-					lposten.setPreis(preis);
-					writer.println(LocalDate.now() + " " + " Wareneingang gebucht (bereits existierender Artikel) von: "
-							+ mitarbeiter.getID() + " (" + mitarbeiter.getName() + ")" + " Artikel: " + artikel.getID()
-							+ " (" + artikel.getName() + ")" + " Bestand :" + bestand + " Preis: " + preis);
-				}
+				artikel = lposten.getArtikel();
+				System.out.println("Der Bestand für den Artikel: " + artikel.getID() + " (" + artikel.getName() + ")"
+						+ " ist: " + lposten.getLagerbestand());
 			}
-			if (!vorhanden) {
-				new Lagerposten(artikel, bestand, preis);
-				writer.println(LocalDate.now() + " " + " Wareneingang gebucht von: " + mitarbeiter.getID() + " ("
-						+ mitarbeiter.getName() + ")" + " Artikel: " + artikel.getID() + " (" + artikel.getName() + ")"
-						+ " Bestand :" + bestand + " Preis: " + preis);
-			}
-
 		} else {
-			System.out.println("Sie haben keine Berechtigung");
+			System.out.println("Lagerposten existiert nicht");
 		}
-		writer.flush();
+	}
+
+	/**
+	 * Bucht einen Wareneingang. Falls der Artikel bereits im Lagersystem existiert,
+	 * aktualisiert die Methode Preis und Bestand.Falls nicht erstellt sie einen
+	 * neuen Lagerposten mit dem Artikel.
+	 * 
+	 * @param mitarbeiter
+	 *            Der Mitarbeiter der den Wareneingang buchen soll.
+	 * @param artikel
+	 *            Der Artikel der gebucht werden soll.
+	 * @param bestand
+	 *            Die Anzahl an Artikeln die gebucht werden sollen.
+	 * @param preis
+	 *            Der Preis mit dem ein Artikel gebucht werden soll.
+	 */
+	public void wareneingangBuchen(Mitarbeiter mitarbeiter, Artikel artikel, int bestand, double preis) {
+		if (artikel != null && mitarbeiter != null) {
+			if (berechtigteMitarbeiter.contains(mitarbeiter.getID())) {
+				boolean vorhanden = false;
+				for (Lagerposten lposten : lagerposten) {
+					if (lposten.getArtikel().equals(artikel)) {
+						vorhanden = true;
+						lposten.setLagerbestand(lposten.getLagerbestand() + bestand);
+						lposten.setPreis(preis);
+						writer.println(LocalDate.now() + " Wareneingang gebucht (bereits existierender Artikel) von: "
+								+ mitarbeiter.getID() + " (" + mitarbeiter.getName() + ")" + " Artikel: "
+								+ artikel.getID() + " (" + artikel.getName() + ")" + " Bestand: " + bestand + " Preis: "
+								+ preis + "€");
+					}
+				}
+				if (!vorhanden) {
+					new Lagerposten(artikel, bestand, preis);
+					writer.println(LocalDate.now() + " Wareneingang gebucht von: " + mitarbeiter.getID() + " ("
+							+ mitarbeiter.getName() + ")" + " Artikel: " + artikel.getID() + " (" + artikel.getName()
+							+ ")" + " Bestand: " + bestand + " Preis: " + preis + "€");
+				}
+
+			} else {
+				System.out.println("Sie haben keine Berechtigung");
+			}
+			writer.flush();
+		} else {
+			System.out.println("Warenbuchung fehlgeschlagen");
+		}
 	}
 
 	/**
 	 * Führt eine Bestellung aus in dem es überprüft ob der jeweilige Artikel zur
-	 * verfügungstehen und die Menge vorhanden ist.
+	 * Verfügung stehen und die Menge vorhanden ist.
 	 * 
-	 * @param mitarbeiter,
-	 *            wird zum überprüfen ob der jeweilige Mitarbeiter recchte zum
+	 * @param mitarbeiter
+	 *            Wird zum Überprüfen ob der jeweilige Mitarbeiter Rechte zum
 	 *            ausführen hat.
-	 * @param bestellposten,
-	 *            enthält die Bestellposten welche bestellt werden sollen (List)
+	 * @param bestellposten
+	 *            Enthält die Bestellposten welche bestellt werden sollen (List)
 	 */
 	public void bestellungAusfuehren(Mitarbeiter mitarbeiter, List<Bestellposten> bestellposten) {
-		Artikel artikel;
-		boolean bestellungausführbar = true;
-		if (berechtigteMitarbeiter.contains(mitarbeiter.getID())) {
-			for (Bestellposten bposten : bestellposten) {
-				if (bestellungausführbar) {
-					for (Lagerposten lposten : lagerposten) {
-						artikel = lposten.getArtikel();
-						if (artikel.getID().equals(bposten.getARTIKELID())) {
-							if (lposten.getLagerbestand() <= bposten.getAnzahl()) {
-								bestellungausführbar = true;
+		if (mitarbeiter != null && bestellposten != null && lagerposten != null) {
+			Artikel artikel;
+			boolean bestellungausführbar = true;
+			if (berechtigteMitarbeiter.contains(mitarbeiter.getID())) {
+				for (Bestellposten bposten : bestellposten) {
+					if (bestellungausführbar) {
+						for (Lagerposten lposten : lagerposten) {
+							artikel = lposten.getArtikel();
+							if (artikel.getID().equals(bposten.getARTIKELID())) {
+								if (lposten.getLagerbestand() <= bposten.getAnzahl()) {
+									bestellungausführbar = true;
+								} else {
+									bestellungausführbar = false;
+								}
 							} else {
 								bestellungausführbar = false;
 							}
-						} else {
-							bestellungausführbar = false;
 						}
 					}
 				}
-			}
-			if (bestellungausführbar) {
-				for (Bestellposten bposten : bestellposten) {
-					for (Lagerposten lposten : lagerposten) {
-						artikel = lposten.getArtikel();
-						if (artikel.getID().equals(bposten.getARTIKELID())) {
-							lposten.setLagerbestand(lposten.getLagerbestand() - bposten.getAnzahl());
+				if (bestellungausführbar) {
+					for (Bestellposten bposten : bestellposten) {
+						for (Lagerposten lposten : lagerposten) {
+							artikel = lposten.getArtikel();
+							if (artikel.getID().equals(bposten.getARTIKELID())) {
+								lposten.setLagerbestand(lposten.getLagerbestand() - bposten.getAnzahl());
+							}
 						}
 					}
+					writer.println(LocalDate.now() + " Bestellung gebucht von Mitarbeiter: " + mitarbeiter.getID()
+							+ " (" + mitarbeiter.getName() + ")");
+					writer.flush();
+				} else {
+					System.out.println("Lagerbestand nicht ausreichend");
 				}
-				writer.println(LocalDate.now() + " " + " Bestellung gebucht von Mitarbeiter: " + mitarbeiter.getID()
-						+ " (" + mitarbeiter.getName() + ")");
-				writer.flush();
+			} else {
+				System.out.println("Sie haben keine Berechtigung");
 			}
+			writer.flush();
 		} else {
-			System.out.println("Sie haben keine Berechtigung");
+			System.out.println("Bestellvorgang fehlgeschlagen");
 		}
-		writer.flush();
 	}
 
 	public void setLagerposten(Lagerposten lagerposten) {
